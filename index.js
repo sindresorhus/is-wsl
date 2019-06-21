7db1c8aab2ead0db0defbd8b834e8a9a008fc1c6
Oleksii Kuchmich <1730837+KyivJunta@users.noreply.github.com>
2019-06-21 21:23:07 +0300

    Remove logs

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ const fs = require('fs');
 
 const isWsl = () => {
 	if (process.platform !== 'linux') {
-		console.log('NOT LINUX');
 		return false;
 	}
 
 	if (os.release().toLocaleLowerCase().includes('microsoft')) {
-		console.log('RELEASE');
 		return true;
 	}
 
 	try {
 		if (fs.readFileSync('/proc/version', 'utf8').toLocaleLowerCase().includes('microsoft')) {
-			console.log('VERSION');
 			return true;
 		}
 	} catch (_) {
